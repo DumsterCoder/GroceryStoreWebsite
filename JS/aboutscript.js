afaa6ff7aab9a495897ebf7e48ddd86880fbba80
Jordan <jordanwenger11@gmail.com>
2023-06-05 15:14:50 -0400

    adjusted JS files and added map

--- JS/aboutscript.js
+++ JS/aboutscript.js
@@ -15,6 +15,17 @@ function dismissList() {
   }
 }
 
+var map = L.map("map").setView([40.798603, -81.695959], 17);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+var marker = L.marker([40.798603, -81.695959]).addTo(map);
+
+marker.bindPopup("EL DESEO");
+
 function init() {
     menuToggle.addEventListener('click', hamburgerBtn);
     restOfPage.addEventListener('click', dismissList);
