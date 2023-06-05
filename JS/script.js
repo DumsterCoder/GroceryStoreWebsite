01c54a177433d8ed896b543ca5d6c0cca94c9f63
Jordan <jordanwenger11@gmail.com>
2023-06-04 21:12:54 -0400

    asjusted home page font and animation

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,13 @@ const restOfPage = document.getElementById('main');
 const languageBtn = document.getElementById('lang-toggle')
 
 
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map).bindPopup('A pretty CSS popup.<br> Easily customizable.').openPopup();
 
 
 
@@ -20,12 +27,9 @@ function dismissList() {
     }
 }
 
-
-
 const init = () => {
     menuToggle.addEventListener('click', hamburgerBtn);
     restOfPage.addEventListener('click', dismissList);
-    languageBtn.addEventListener('click', newLanguage);
 }
 
 init();
