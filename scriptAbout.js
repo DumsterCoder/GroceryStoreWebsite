2d8e49c51faf51665dcbfd306264c976a2136884
Jordan <jordanwenger11@gmail.com>
2023-06-22 14:03:38 -0400

    added base template for pop-up product info block

--- scriptAbout.js
+++ scriptAbout.js
@@ -2,6 +2,10 @@ const menuToggle = document.getElementById("menu-toggle");
 const menuList = document.getElementById("menu-list");
 const restOfPage = document.getElementById("main");
 const languageBtn = document.getElementById("lang-toggle");
+const tomato = {
+  image: 'https://bioweed.com.au/wp-content/uploads/Eight-tomato-types-grown-in-australia.png';
+  title: 'Brandyine Tomatoes'
+}
 
 function hamburgerBtn() {
   menuList.classList.toggle("hidden");
